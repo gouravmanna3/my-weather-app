84e15361c23adcc7ba6b8f86ea3aa94e0f7f5f8a
Gourav Manna <gmanna@intralinks.com>
2021-07-23 00:19:03 +0530

    feat: integrated the forecast api

--- src/features/forecast/index.js
+++ src/features/forecast/index.js
@@ -1,39 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { WeatherContext } from '../weather/index';
+import WeatherIcon from '../weather/weatherIcon';
 
 import './forecast.css';
 
 const Forecast = () => {
+  const weatherContext = useContext(WeatherContext);
+  const { forecastData: { city, list: hourlyWeather } } = weatherContext;
   return (
     <div className="forecastContainer">
       <header>Forecast</header>
       <section className="hourTemp">
-        {/* <section>
-          Hello
-        </section>
-        <section>
-        Hello
-        </section>
-        <section>
-        Hello
-        </section>
-        <section>
-          Hello
-        </section>
-        <section>
-        Hello
-        </section>
-        <section>
-        Hello
-        </section>
-        <section>
-          Hello
-        </section>
-        <section>
-        Hello
-        </section>
-        <section>
-        Hello
-        </section> */}
+      { hourlyWeather?.map((hourWeather,key) => (
+        
+          <section key={key}>
+            <WeatherIcon  iconData={hourWeather?.weather[0]} className='hourlyIcon'/>
+            
+            <h4>{parseInt(hourWeather?.main?.temp)}&deg;c</h4>
+          </section>
+        
+      ))}
       </section>
     </div>
   )
