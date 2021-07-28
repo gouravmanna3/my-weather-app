a6cfac43e18359be1ca2d574629cb6b419d72ab0
Gourav Manna <gmanna@intralinks.com>
2021-07-29 00:21:18 +0530

    feat: added loading spinner and removed forecast weather

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -9,8 +9,8 @@ import {
   Navbar,
   NavbarBrand,
 } from 'reactstrap';
-import Forecast from '../forecast/index';
-import logo from '../../assets/images/logo.gif'
+import logo from '../../assets/images/logo.gif';
+import Loader from "react-loader-spinner";
 
 
 import './weather.css';
@@ -19,6 +19,7 @@ import './weather.css';
 const initialState = {
   city: '',
   weatherData: [],
+  weatherDataApiStatus: 'notStarted',
   forecastData: []
 };
 
@@ -36,6 +37,10 @@ const reducer = (state, action) => {
       return {
         ...state, forecastData: action.payload
       }
+    case 'SET_WEATHER_DATA_API_STATUS':
+      return {
+        ...state, weatherDataApiStatus: action.payload
+    }
     default:
       return state;
   }
@@ -64,40 +69,41 @@ const Weather = () => {
   }
 
   const callApi = () => {
-    axios.all([
-      axios.get(`${process.env.REACT_APP_API_URL}/weather/?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&cnt=10&APPID=${process.env.REACT_APP_API_KEY}`),
-      axios.get(`${process.env.REACT_APP_API_URL}/forecast?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&cnt=10&APPID=${process.env.REACT_APP_API_KEY}`),
-    ])
-    .then(axios.spread((weather, forecast) => {
-      dispatch({type: 'SET_WEATHER_DATA', payload: weather.data})
-      dispatch({type: 'SET_FORECAST_DATA', payload: forecast.data})
-    }))
+    dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'inProgress'})
+    axios.get(`${process.env.REACT_APP_API_URL}/weather/?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&cnt=10&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => {
+      dispatch({type: 'SET_WEATHER_DATA', payload: res.data})
+      dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'found'})
+    })
     .catch(err => {
-      console.log('error')
+      dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'error'})
     })
   }
 
   const cityWeather = (e) => {
     e.preventDefault();
+    dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'inProgress'})
     const { city } = state;
     axios.get(`${process.env.REACT_APP_API_URL}/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
     .then(response => {
       dispatch({type: 'SET_WEATHER_DATA', payload: response.data});
+      dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'found'})
     })
     .catch(err => {
-      console.log(err);
+      dispatch({type: 'SET_WEATHER_DATA_API_STATUS', payload: 'error'})
     })
   }
 
   useEffect(() => {
     fetchData();
-    callApi();
+    if(coordinates.lat && coordinates.long) {
+      callApi();
+    }
   },[coordinates.lat, coordinates.long]);
 
-  const { weatherData } = state;
+  const { weatherData, weatherDataApiStatus } = state;
   const { forecastData } = state;
   const iconData = weatherData?.weather ? weatherData?.weather[0] : {};
-  console.log()
   return (
     <main>
       <Navbar className="container-fluid" color="transparent" full={true} light expand="md">
@@ -109,53 +115,65 @@ const Weather = () => {
       <WeatherContext.Provider value={{ state, dispatch, cityWeather }}>
         <SearchCity />
       </WeatherContext.Provider>
-      <div className="weather-container">
-        <article className="header">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="locationIcon"/>
-          <span> {weatherData?.name}, {weatherData?.sys?.country}</span>
-          <h4>{moment().format('dddd MMMM Do, YYYY')}</h4>
-        </article>
-        
-        <section className="weather">
-          <article className="desc">
-            <WeatherIcon  iconData={iconData} className='icon'/>
-            <span className="descText">{ weatherData?.weather ? weatherData?.weather[0].description : ''}</span>
-          </article>
-          <article className="temp">
-            <span>{parseInt(weatherData.main?.temp)}&deg;c</span>
-          </article>
-          <article className="detailsContainer">
-            <article>
-              <h4>{parseInt(weatherData?.main?.temp_min)}&deg;c</h4>
-              <span>Low</span>
+      { weatherDataApiStatus === 'inProgress' || weatherDataApiStatus === 'notStarted' ?
+          <div className="loading">
+            <Loader
+              type="TailSpin"
+              color="#FFF"
+              height={100}
+              width={100}
+            />
+          </div>
+           : weatherDataApiStatus === 'error' ? 
+            <h1>City not found</h1>
+           :
+          <div className="weather-container">
+            <article className="header">
+              <FontAwesomeIcon icon={faMapMarkerAlt} className="locationIcon"/>
+              <span> {weatherData?.name}, {weatherData?.sys?.country}</span>
+              <h4>{moment().format('dddd MMMM Do, YYYY')}</h4>
             </article>
-            <article>
-              <h4>{weatherData?.wind?.speed} m/s</h4>
-              <span>Wind</span>
-            </article>
-            <article>
-              <h4>{weatherData.main?.humidity}%</h4>
-              <span>Humidity</span>
-            </article>
-            <article>
-              <h4>{parseInt(weatherData?.main?.temp_max)}&deg;c</h4>
-              <span>High</span>
-            </article>
-            <article>
-              <h4>{new Date(weatherData?.sys?.sunrise*1000).toLocaleTimeString('en-IN',{hour: '2-digit', minute:'2-digit'})}</h4>
-              <span>Sunrise</span>
-            </article>
-            <article>
-              <h4>{new Date(weatherData?.sys?.sunset*1000).toLocaleTimeString('en-IN',{hour: '2-digit', minute:'2-digit'})}</h4>
-              <span>Sunset</span>
-            </article>
-          </article>
-        </section>
-      </div>
-      <WeatherContext.Provider value={{forecastData}}>
+            
+            <section className="weather">
+              <article className="desc">
+                <WeatherIcon  iconData={iconData} className='icon'/>
+                <span className="descText">{ weatherData?.weather ? weatherData?.weather[0].description : ''}</span>
+              </article>
+              <article className="temp">
+                <span>{parseInt(weatherData.main?.temp)}&deg;c</span>
+              </article>
+              <article className="detailsContainer">
+                <article>
+                  <h4>{parseInt(weatherData?.main?.temp_min)}&deg;c</h4>
+                  <span>Low</span>
+                </article>
+                <article>
+                  <h4>{weatherData?.wind?.speed} m/s</h4>
+                  <span>Wind</span>
+                </article>
+                <article>
+                  <h4>{weatherData.main?.humidity}%</h4>
+                  <span>Humidity</span>
+                </article>
+                <article>
+                  <h4>{parseInt(weatherData?.main?.temp_max)}&deg;c</h4>
+                  <span>High</span>
+                </article>
+                <article>
+                  <h4>{new Date(weatherData?.sys?.sunrise*1000).toLocaleTimeString('en-IN',{hour: '2-digit', minute:'2-digit'})}</h4>
+                  <span>Sunrise</span>
+                </article>
+                <article>
+                  <h4>{new Date(weatherData?.sys?.sunset*1000).toLocaleTimeString('en-IN',{hour: '2-digit', minute:'2-digit'})}</h4>
+                  <span>Sunset</span>
+                </article>
+              </article>
+            </section>
+          </div>
+        } 
+      {/* <WeatherContext.Provider value={{forecastData}}>
         <Forecast />
-      </WeatherContext.Provider>
-
+      </WeatherContext.Provider> */}
     </main>
   )
 }
